fc59acc44a59643ffac0cca2991601554c39c6ba
y_yagi <yuuji.yaginuma@gmail.com>
2012-01-29 15:49:01 +0900

    first commit

---
+++ db/seeds.rb
@@ -0,0 +1,10 @@
+# -*- encoding: UTF-8 -*-
+
+User.create(
+  :twitter_id => 'y_yagi', 
+  :initial_weight => 60.2, 
+  :initial_body_fat_percentageinitial => 18.0,
+  :regist_date => Date.today
+)
+
+
