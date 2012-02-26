540df5ecdcd4d35cd774c02bac418fa437193c0c
y_yagi <yuuji.yaginuma@gmail.com>
2012-02-26 15:26:43 +0900

    test

--- Gemfile
+++ Gemfile
@@ -20,7 +20,9 @@ group :assets do
   gem 'uglifier', '>= 1.0.3'
 end
 
-gem 'jquery-rails'
+gem 'jquery-rails' 
+
+gem 'oauth'
 
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
---
+++ initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WeightHistory::Application.config.secret_token = '487201cbed217b1f4246b69df44332f2eade52b17d2e48286fc2fd856ddac087cd0fa3cabf6d18936ecc575f22725a3a9c2a3af0730881a63ad52a9adf3b9bf5'
